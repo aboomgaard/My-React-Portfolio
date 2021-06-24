25f4acf01a6558efc659ef7f51f74e14094942bf
Amalia Boomgaard <42347275+aboomgaard@users.noreply.github.com>
2021-06-24 00:17:27 -0400

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,10 +5,10 @@ function Footer() {
         <div id="contact">
         <h2>Contact</h2>
         <ul>
+         <a a href="https://www.linkedin.com/in/amalia-boomgaard-126790172/" target="_blank">LinkedIn</a>
+         <a a href="https://github.com/aboomgaard" target="_blank">Github</a>
           <li>(239)771-3312</li>
-          <li>amaliaboomgardr@gmail.com</li>
-          <a a href="https://github.com/aboomgaard" target="_blank">Github</a>
-          <a a href="https://www.linkedin.com/in/amalia-boomgaard-126790172/" target="_blank">LinkedIn</a>
+          <li>amaliaboomgaard@gmail.com</li>      
           <li>Made by Amy Boomgaard ♥</li>
         </ul>
       </div>
